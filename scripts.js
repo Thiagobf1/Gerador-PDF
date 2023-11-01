bed438a7994e43e4c95ee1f9f5dc15903973d852
Thiagobf1 <thiago.busifeijo@gmail.com>
2023-11-01 13:31:46 -0300

    Config PDF

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
 const btnGenerate = document.querySelector('#Generate-PDF');
 
-brn
+btnGenerate.addEventListener('click', () => {
+
+  // Conteúdo PDF
+  const content = document.querySelector("#content");
+
+  // Configuração PDF
+  const options = {
+    margin: [10, 10, 10, 10],
+    filename: "arquivo.pdf",
+    html2canvas: {scale: 2},
+    jsPDF: {unit: "mm", format: "a4", orientation: "portrait"}
+  }
+
+  // Gerar e baixar PDF
+  html2pdf().set(options).from(content).save();
+});
